54a6496ef34048e97207e9d5f2c5d93fafc8fbfa
Nasif <nasifn@gmail.com>
2014-02-23 16:27:27 +0530

    god must be crazy

---
+++ SampleUIDesigns/src/com/example/sampleuidesigns/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sampleuidesigns;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_screen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void onClick(View v) {
+		// code for clicking Login on UI
+	}
+
+}
